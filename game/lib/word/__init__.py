d76f03acad147384e08ba8740fbcb1a3d8ba6e85
Andrieli Luci <andrieliluci@gmail.com>
2021-09-11 09:56:38 -0300

    Update the words

--- game/lib/word/__init__.py
+++ game/lib/word/__init__.py
@@ -4,15 +4,27 @@ from random import choice, randint
 def chooseWord():
     words = {'food': ['apple', 'avocado', 'banana', 'beet', 'blueberry', 'bacon', 'bread', 'broccoli',
                       'cucumber', 'cheese', 'jellybean', 'lemon', 'lime', 'milk', 'potato', 'rice', 'sausage',
-                      'strawberry', 'soda'],
-             'animal': ['cat', 'cow', 'dog', 'fish', 'frog', 'horse', 'pig', 'turtle'],
-             'color': ['blue', 'yellow', 'green', 'red', 'purple', 'pink', 'gray', 'orange', 'white', 'black',
-                       'brown', 'turquoise'],
-             'object': ['pen', 'cup', 'notebook', 'paper', 'shoe', 'glasses', 'book'],
-             'place': ['house', 'hospital', 'restaurant', 'bookstore', 'library', 'factory', 'movie theater', 'museum',
-                       'gas station', 'school', 'office']}
+                      'strawberry', 'soda', 'coconut', 'coffee', 'cake', 'cabbage', 'donut', 'egg', 'french fries',
+                      'fungi', 'grape', 'guava', 'hamburger', 'honey', 'hot dog', 'lollipop', 'mango', 'melon',
+                      'orange', 'onion', 'pie', 'tomato', 'watermelon', 'yogurt', 'zucchini'],
 
-    theme = choice(['food', 'animal', 'color', 'object'', place'])
+             'animal': ['cat', 'cow', 'dog', 'fish', 'frog', 'horse', 'pig', 'turtle', 'penguin', 'elephant', 'lion',
+                        'bee', 'monkey', 'alligator', 'snake', 'wolf', 'tiger', 'spider', 'worm', 'mice', 'mouse',
+                        'butterfly', 'chicken', 'crab', 'crocodile', 'dragonfly', 'camel', 'duck', 'owl', 'whale',
+                        'shark', 'bat', 'iguana', 'bear', 'giraffe', 'jellyfish', 'ladybird'],
+
+             'color': ['blue', 'yellow', 'green', 'red', 'purple', 'pink', 'gray', 'orange', 'white', 'black',
+                       'brown', 'turquoise', 'fuchsia', 'gold', 'lilac'],
+
+             'object': ['pen', 'cup', 'notebook', 'paper', 'shoe', 'glasses', 'book', 'air conditioner', 'bag',
+                        'bed', 'chair', 'computer', 'dishwasher', 'lamp', 'microwave', 'towel', 'umbrella',
+                        'vacuum cleaner', 'television'],
+
+             'place': ['house', 'hospital', 'restaurant', 'bookstore', 'library', 'factory', 'movie theater', 'museum',
+                       'gas station', 'school', 'office', 'supermarket', 'hotel', 'fire department', 'church', 'square',
+                       'cafe', 'bank', 'pharmacy', 'hair salon', 'barber shop', 'laundry']}
+
+    theme = choice(['food', 'animal', 'color', 'object', 'place'])
     item = randint(0, (len(theme) - 1))
     word = words[theme][item]
 
